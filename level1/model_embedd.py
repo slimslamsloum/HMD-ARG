5ce379700ba4df0d41d12ba231a582b85e820564
slimslamsloum <selim.jerad@epfl.ch>
2022-08-15 16:05:21 +0100

    feat: added level1 from before

---
+++ level1/model_embedd.py
@@ -0,0 +1,108 @@
+"""
+
+"""
+
+from collections import Counter
+from plistlib import load
+from tkinter import Y
+
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn.functional as F
+from biodatasets import list_datasets, load_dataset
+from deepchain.models import MLP
+from deepchain.models.utils import (confusion_matrix_plot,
+                                    model_evaluation_accuracy)
+from multitaskmodel_embedd import MultiTaskModelEmbedd
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from torch import Tensor, float32, float64
+from torch.utils.data import DataLoader
+from torchmetrics import F1, Accuracy, AveragePrecision, Precision, Recall
+
+# Load embedding and target dataset
+dataset = load_dataset("antibiotic-resistance")
+
+X, y = dataset.to_npy_arrays(input_names=["sequence"], target_names=["label"])
+X, atb_classes = dataset.to_npy_arrays(input_names=["sequence"], target_names=["antibiotic_class"])
+X, mechanism = dataset.to_npy_arrays(input_names=["sequence"], target_names=["mechanism"])
+y = y[0]
+
+# Compute the mechanisms associated to each protein
+# There are 5 different resistance mechanisms
+atb_classes = atb_classes[0][y==1]
+encoder_1 = preprocessing.LabelEncoder()
+encoded_atb_classes = encoder_1.fit_transform(atb_classes)
+#print(len((encoded_atb_classes)))
+
+#5 resistance mechanisms
+mechanism = mechanism[0][y==1]
+encoder_2 = preprocessing.LabelEncoder()
+encoded_mechanism = encoder_2.fit_transform(mechanism)
+#print(len((encoded_mechanism)))
+
+
+#8706 samples that are ARG
+
+# Compute embeddings (of size 1280)
+
+cls_embeddings = np.load(
+        "/home/selim/.cache/bio-datasets/antibiotic-resistance/sequence_esm1_t34_670M_UR100_cls_embeddings.npy",
+    allow_pickle=True)[y==1]
+
+cls_embeddings = np.vstack(cls_embeddings).astype(np.float)
+
+# Separate in training and test set. Compute dataloaders
+x_train, x_test, y_train, y_test = train_test_split(cls_embeddings, np.column_stack((encoded_atb_classes, encoded_mechanism)), test_size=0.3)
+batch_size = 32
+trainloader = DataLoader(np.column_stack((x_train, y_train)), batch_size=batch_size)
+testloader = DataLoader(np.column_stack((x_test, y_test)), batch_size=batch_size)
+
+#print(np.column_stack((x_train, y_train)))
+
+# Create model
+model = MultiTaskModelEmbedd()
+
+# Train model
+model.training_step(trainloader=trainloader, nb_epochs=10, atb_coeff=0.1, mech_coeff=1.2)
+
+# Save model
+model.save_model(path = "/home/selim/Documents/myApps/HMD-ARG/src/model_embedd")
+
+# Predict on test set
+atb_pred, mech_pred = model.forward(x_test)
+
+pred=[]
+for i in range(len(x_test)):
+    #print(np.argmax(mech_pred[i].detach().numpy))
+    pred.append((np.argmax(atb_pred[i].detach().numpy()),np.argmax(mech_pred[i].detach().numpy())))
+
+#print(pred)
+
+# Accuracy evaluation
+accuracy = Accuracy(mdmc_average='samplewise')
+print('Accuracy: {0}'.format(accuracy(torch.tensor(pred), torch.from_numpy(y_test))))
+
+# Precision evaluation
+precision = Precision(mdmc_average='samplewise')
+print('Precision: {0}'.format(precision(torch.tensor(pred), torch.from_numpy(y_test).int())))
+
+# Recall evalution
+recall = Recall(mdmc_average='samplewise')
+print('Recall: {0}'.format(recall(torch.tensor(pred), torch.from_numpy(y_test).int())))
+
+# F1 Score evaluation
+f1score = F1(mdmc_average='samplewise')
+print('F1 Score: {0}'.format(f1score(torch.tensor(pred), torch.from_numpy(y_test).int())))
+
+
+
+
+
+
+
+
+
+
+
---
+++ level1/multitaskmodel_embedd.py
@@ -0,0 +1,72 @@
+import enum
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+from deepchain.models.torch_model import TorchModel
+from sklearn.preprocessing import OneHotEncoder
+from torch import nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+
+
+class MultiTaskModelEmbedd(TorchModel):
+
+    # Model architecture
+    def __init__(self, input_shape: int = 1280, **kwargs):
+        super().__init__(**kwargs)
+
+        self.dropout = nn.Dropout(0.9)
+
+        self.fc1 = nn.Linear(1280, 1024)
+        self.fc2 = nn.Linear(1024,1024)
+    
+        self.output_atb_class = nn.Sequential(nn.Linear(1024, 25), nn.Softmax())
+        self.output_mechanism = nn.Sequential(nn.Linear(1024,5), nn.Softmax())
+        self.output_mobility = nn.Sequential(nn.Linear(1024,2), nn.Softmax())
+        
+        self.hidden = nn.Sequential(self.fc1, nn.ReLU(), self.fc2)
+
+        self.loss = F.cross_entropy
+        self.optimizer = optim.Adam(self.parameters(), lr=0.0001)
+
+    # Forward pass
+    def forward(self, x):
+        x = torch.tensor(x).float()
+        x = self.dropout(x)
+        hidd = self.hidden(x)
+        atb_class = self.output_atb_class(hidd)
+        mechanism = self.output_mechanism(hidd)
+        #mobility = self.output_mobility(hidd)
+
+        return atb_class, mechanism #, mobility
+
+    # Train model
+    def training_step(self, trainloader, nb_epochs, atb_coeff, mech_coeff):
+        for epoch in range(nb_epochs):
+
+            for (idx, batch) in enumerate(trainloader):
+                x = batch[:,:-2]
+                y = batch[:,-2:]
+
+                self.optimizer.zero_grad()
+
+                #atb_class_pred, mechanism_pred, mobility_pred = self.forward(inputs)
+                atb_class_pred, mechanism_pred = self.forward(x.unsqueeze(1))
+                
+                #print(np.argmax(atb_class_pred.detach().numpy(), axis = 1))
+                #print(y[:,0])
+                
+                loss_atb_class = self.loss(atb_class_pred.squeeze(1), y[:,0].type(torch.LongTensor))
+                loss_mechanisms = self.loss(mechanism_pred.squeeze(1), y[:,1].type(torch.LongTensor))
+
+                loss = loss_atb_class*atb_coeff + loss_mechanisms*mech_coeff
+
+                loss.backward()
+                
+                self.optimizer.step()
+
+    # Save model
+    def save_model(self, path: str):
+        torch.save({'model': self.state_dict()}, path)   
